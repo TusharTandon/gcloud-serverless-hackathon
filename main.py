dc0b9fb08e5d410b0ea4b6fa41576eb07f3864d7
TusharTandon <tushartandon14@gmail.com>
2021-10-03 17:39:13 +0000

    init

---
+++ main.py
@@ -0,0 +1,47 @@
+
+# Copyright 2020 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import logging
+import random
+from flask import Flask, request
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+logger = logging.getLogger(__name__)
+
+app = Flask(__name__)
+moves = ['F', 'T', 'L', 'R']
+dirn=['R','T']
+water=['T']
+# dirn=['R','R']
+
+def foot():
+    return moves[random.randrange(len(moves))]
+
+@app.route("/", methods=['POST'])
+def move():
+    request.get_data()
+    # request.json
+    logger.info(request.json)
+    # return moves[random.randrange(len(moves))]
+    # return moves[1]
+    return moves[1], foot(),dirn,water
+
+# def throw():
+#     return 
+
+if __name__ == "__main__":
+  app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+  
